26031168c9d33af89bd95415506728125ef9a004
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-03 19:56:15 +0300

    refactor

--- src/pages/Balance/Balance.tsx
+++ src/pages/Balance/Balance.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import styles from './Balance.module.scss';
+import CircleButton from "../../components/ui/CircleButton/CircleButton";
+import { useNavigate } from "react-router-dom";
 
 const Balance: FC = () => {
+  const navigate = useNavigate();
 
   return (
   <div className={styles.balance}>
     <div className={styles.balance__content}>
+    <button onClick={() => navigate(-1)} className={styles.balance__chevron}>
+    <CircleButton chevronPosition="left" />
+    </button>
       <div className={styles.balance__amount}>
         <h3 className={styles.balance__h3}>0</h3>
         <h3 className={styles.balance__h3}>Баллов</h3>
--- src/utils/mockData.ts
+++ src/utils/mockData.ts
@@ -2,17 +2,17 @@ export const bannersData = [
   {
     backgroundImage: 'url("https://img.freepik.com/free-photo/young-brunette-in-white-casual-sweater-isolated-on-purple-wall_343596-5603.jpg?w=1800&t=st=1709298098~exp=1709298698~hmac=fd4726320de1ce15ec0366de16a30e5f18fb2d7528571c3583de3b008cc3f267")',
     text: 'Зови друзей! + 500!',
-    link: '/balance'
+    link: '/rooms'
   },
   {
     backgroundImage: 'url("https://img.freepik.com/free-photo/isolated-shot-of-satisfied-male-model-stands-sideways-in-paper-hole-dressed-in-yellow-headgear_273609-25560.jpg?w=1800&t=st=1709298094~exp=1709298694~hmac=a6e66b3c59c7a8f9997613463f49b339a86e1116dcc923b464ddcb6008ca3302")',
     text: 'Привет, корешок',
-    link: '/balance'
+    link: '/rooms'
   },
   {
     backgroundImage: 'url("https://img.freepik.com/free-photo/embarrassed-emotional-red-haired-woman-stands-with-open-mouth-bugged-eyes-points-right-at-copy-space-through-paper-hole_273609-46490.jpg?w=1800&t=st=1709298092~exp=1709298692~hmac=28db23d2f81885c0d1d23655a37b4aa9381c2012969a2a7b6d933a784468a267")',
     text: 'Это слайдер номер 3, проверяем',
-    link: '/balance'
+    link: '/rooms'
   },
 ];
 
