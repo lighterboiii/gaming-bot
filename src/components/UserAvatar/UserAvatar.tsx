1d268de511be11227b090197d963e378843c4c48
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-07 14:40:53 +0300

    another one

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC, useEffect, useState } from "react";
-import { getSkinAndMaskByIndex } from "../../utils/getSkinAndMask";
+import { getRandomSkinAndMask, getSkinAndMaskByIndex } from "../../utils/getSkinAndMask";
 import styles from './UserAvatar.module.scss';
 import avatar from '../../images/griffin.jpeg';
 import { useAppSelector } from "../../services/reduxHooks";
 
 const UserAvatar: FC = () => {
-  const [skin, setSkin] = useState<any>(null);
-  const [mask, setMask] = useState<any>(null);
+  const [userSkin, setSkin] = useState<any>(null);
+  const [userMask, setMask] = useState<any>(null);
+  const { skin, mask } = getRandomSkinAndMask();
   const userData = useAppSelector(store => store.user.userData);
-
+  console.log(userData);
   useEffect(() => {
     if (userData && userData.info.active_skin !== undefined) {
       const { skin: selectedSkin, mask: selectedMask } = getSkinAndMaskByIndex(userData.info.active_skin);
@@ -20,17 +21,17 @@ const UserAvatar: FC = () => {
 
   return (
     <>
-      {skin && mask && (
-        <div className={styles.userAvatar}>
-          <div className={styles.userAvatar__avatarBackground} style={{ backgroundImage: `url(${skin})` }}></div>
-          <img
-            src={avatar}
-            alt="user_avatar"
-            className={styles.userAvatar__userAvatar}
-            style={{ maskImage: `url(${mask})` }}
-          />
-        </div>
-      )}
+      <div className={styles.userAvatar}>
+        <div
+          className={styles.userAvatar__avatarBackground}
+          style={{ backgroundImage: `url(${userData?.info ? userSkin : skin})` }}></div>
+        <img
+          src={avatar}
+          alt="user_avatar"
+          className={styles.userAvatar__userAvatar}
+          style={{ maskImage: `url(${userData?.info ? userMask : mask})` }}
+        />
+      </div>
     </>
   )
 }
