1f7b660db7c0dd67233e0f4afee645526ddf85e1
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-15 13:02:30 +0300

    L

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from './Shop.module.scss';
 import UserInfo from "../../components/SecondaryUserInfo/SecondaryUserInfo";
 import CircleButton from "../../components/ui/CircleButton/CircleButton";
@@ -11,8 +11,15 @@ import { useAppSelector } from "../../services/reduxHooks";
 const Shop: FC = () => {
   const navigate = useNavigate();
   const [goods, setGoods] = useState(shopItems);
+  const [activeButton, setActiveButton] = useState('Магазин');
   // const userSkins = useAppSelector(store => store.user.userData?.info.collectibles);
   const userSkins = [1, 2, 5];
+
+  useEffect(() => {
+    setActiveButton('Магазин');
+  }, []);
+
+
   const handleShowCollectibles = () => {
     if (userSkins.length > 0) {
       const filteredItems = shopItems.filter((item: any) => userSkins.includes(item.id));
@@ -20,19 +27,22 @@ const Shop: FC = () => {
     } else {
       setGoods(shopItems);
     }
+    setActiveButton('Приобретено');
   };
 
   const handleCancelSort = () => {
     setGoods(shopItems);
+    setActiveButton('Магазин');
   };
 
   const handleShowFlea = () => {
     setGoods(userSkinsForSale);
+    setActiveButton('Лавка');
   }
 
   return (
     <div className={styles.shop}>
-      <div style={{ position: 'absolute', top: '24px', left: '24px' }} onClick={() => navigate(-1)}>
+      <div style={{ position: 'absolute', top: '16px', left: '16px' }} onClick={() => navigate(-1)}>
         <CircleButton chevronPosition="left" color="#d51845" isWhiteBackground chevron />
       </div>
       <div className={styles.shop__header}>
@@ -42,19 +52,19 @@ const Shop: FC = () => {
       <div className={styles.shop__buttons}>
         <div className={styles.shop__leftButtonsContainer}>
           <button 
-          className={styles.shop__button} 
+          className={`${styles.shop__button} ${activeButton === 'Магазин' ? styles.activeButton : ''}`} 
           onClick={handleCancelSort}>
             Магазин
           </button>
           <button 
-          className={styles.shop__button}
+          className={`${styles.shop__button} ${activeButton === 'Лавка' ? styles.activeButton : ''}`} 
           onClick={handleShowFlea}
           >
             Лавка
           </button>
         </div>
         <button
-          className={styles.shop__buttonWhite}
+          className={`${styles.shop__button} ${activeButton === 'Приобретено' ? styles.activeButton : ''}`} 
           onClick={handleShowCollectibles}
         >
           Приобретено
