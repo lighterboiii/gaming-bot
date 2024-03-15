a7404787f1729325596609c111c64facf17c0021
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-15 15:16:43 +0300

    добавить проверку на загрузку данных

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Routes, Route } from 'react-router-dom';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './App.module.scss';
 import CreateRoom from '../../pages/CreateRoom/CreateRoom';
 import Main from '../../pages/Main/Main';
@@ -19,6 +19,7 @@ import Game from '../../pages/Game/Game';
 
 const App: FC = () => {
   const { tg, user } = useTelegram();
+  const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -29,13 +30,14 @@ const App: FC = () => {
   }, []);
 
   useEffect(() => {
+    setLoading(true);
     const fetchUserData = async () => {
       try {
         // const userDataResponse = await getReq<UserData>({ uri: getUserInfoUri, userId: user?.id });
         const userDataResponse = await getReq<UserData>({ uri: getUserInfoUri, userId: userId });
         // const userPhotoResponse = await getReq<any>({ uri: getUserPhotoUri, userId: userId });
         dispatch(setUserData(userDataResponse));
-
+        setLoading(false);
       } catch (error) {
         console.error('Ошибка в получении данных пользователя:' + error);
       }
@@ -46,14 +48,17 @@ const App: FC = () => {
 
   return (
     <div className={styles.app}>
+      {loading ?  <div>Загрузка...</div> : (
       <Routes>
-        <Route path={indexUrl} element={<Main />} />
-        <Route path={roomsUrl} element={<Rooms />} />
-        <Route path={createRoomUrl} element={<CreateRoom />} />
-        <Route path={shopUrl} element={<Shop />} />
-        <Route path={leaderboardUrl} element={<LeaderBoard />} />
-        {/* <Route path='/game' element={<Game />} /> */}
-      </Routes>
+      <Route path={indexUrl} element={<Main />} />
+      <Route path={roomsUrl} element={<Rooms />} />
+      <Route path={createRoomUrl} element={<CreateRoom />} />
+      <Route path={shopUrl} element={<Shop />} />
+      <Route path={leaderboardUrl} element={<LeaderBoard />} />
+      {/* <Route path='/game' element={<Game />} /> */}
+    </Routes>
+      )
+}
     </div>
   );
 }
