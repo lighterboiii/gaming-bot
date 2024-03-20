d4779ceed1c770511aaa8269bf4ea7751e7fbf21
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-21 01:21:39 +0300

    loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
 import styles from './Loader.module.scss';
+import logoAnimated from '../../images/loader.png';
 
 const Loader: FC = () => {
   return (
-    <div className={styles.loader}>Загрузка...</div>
+    <div className={styles.loader}>
+      <img src={logoAnimated} alt="loading" />
+      <p className={styles.loader__text}>Загрузка...</p>
+    </div>
   )
 };
 
