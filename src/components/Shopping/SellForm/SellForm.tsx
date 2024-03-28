ff12095b7f1f73ea59a6a2f4104c0adc8fd5b44d
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-29 00:05:36 +0300

    M

--- src/components/Shopping/SellForm/SellForm.tsx
+++ src/components/Shopping/SellForm/SellForm.tsx
@@ -19,7 +19,7 @@ interface IProps {
 
 const SellForm: FC<IProps> = ({ item, setMessageShown, setMessage, onClose }) => {
   const { user } = useTelegram();
-  const userId = user?.id;
+  // const userId = user?.id;
   const dispatch = useAppDispatch();
 
   const [priceValue, setPriceValue] = useState('')
