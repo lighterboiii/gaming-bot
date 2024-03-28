cb4c63e7daab17976aaf2dc78991bfddf4ec3c73
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-03-28 20:59:13 +0300

    yo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -24,7 +24,7 @@ const App: FC = () => {
   const [loading, setLoading] = useState(false);
   const dispatch = useAppDispatch();
 
-  document.addEventListener('touchmove', (event: TouchEvent) => {
+  document.addEventListener('touchstart', (event: TouchEvent) => {
     if ((event.target as Element).classList.contains('scrollable')) {
       return;
     }
