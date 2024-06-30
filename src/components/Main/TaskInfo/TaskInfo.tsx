5aafa2e0bf48c644d33a04570107564c4d44cb20
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-06-30 21:33:08 +0300

    ID

--- src/components/Main/TaskInfo/TaskInfo.tsx
+++ src/components/Main/TaskInfo/TaskInfo.tsx
@@ -19,7 +19,7 @@ interface IProps {
 
 const TaskInfo: FC<IProps> = ({ task, setSelectedTask }) => {
   const { tg, user } = useTelegram();
-  // const userId = user?.id;
+  const userId = user?.id;
   const dispatch = useAppDispatch();
   const translation = useAppSelector(store => store.app.languageSettings);
   const [showReward, setShowReward] = useState<boolean>(false);
