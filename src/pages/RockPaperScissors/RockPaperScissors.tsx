ca0b5966fb6fcd2d766cb9dcc52dc9ecc42c51d3
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-07-11 23:51:32 +0300

    Id

--- src/pages/RockPaperScissors/RockPaperScissors.tsx
+++ src/pages/RockPaperScissors/RockPaperScissors.tsx
@@ -36,7 +36,7 @@ import styles from "./RockPaperScissors.module.scss";
 export const RockPaperScissors: FC = () => {
   const navigate = useNavigate();
   const { tg, user } = useTelegram();
-  // const userId = user?.id;
+  const userId = user?.id;
   const { roomId } = useParams<{ roomId: string }>();
   const dispatch = useAppDispatch();
   const [data, setData] = useState<any>(null);
