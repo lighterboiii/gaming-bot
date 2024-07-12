ab27e2653e3fc098b0621a09d222e4c828332542
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-07-12 15:13:10 +0300

    deploy?

--- src/components/Game/JoinRoomPopup/JoinRoomPopup.tsx
+++ src/components/Game/JoinRoomPopup/JoinRoomPopup.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
