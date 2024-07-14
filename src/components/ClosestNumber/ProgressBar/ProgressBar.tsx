87c90219c4aa2130b707feb1133c508fb5db79da
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-07-14 18:32:25 +0300

    anim

--- src/components/ClosestNumber/ProgressBar/ProgressBar.tsx
+++ src/components/ClosestNumber/ProgressBar/ProgressBar.tsx
@@ -16,13 +16,13 @@ const CircularProgressBar: FC<IProps> = ({ progress }) => {
       const angle = ((limitedProgress * 3.6) - 90) * (Math.PI / 180);
       const x = 50 + 45 * Math.cos(angle);
       const y = 50 + 45 * Math.sin(angle);
-  
+
       setIsAnimating(true);
       const animationTimeout = setTimeout(() => {
         setOffset({ x, y });
         setIsAnimating(false);
       }, 4000);
-  
+
       return () => clearTimeout(animationTimeout);
     }
 
@@ -38,70 +38,25 @@ const CircularProgressBar: FC<IProps> = ({ progress }) => {
         cy={offset.y}
         r="10"
       >
+        <defs>
+          <filter id="blurFilter">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" />
+          </filter>
+        </defs>
       </circle>
-      <text 
-      x="50%" 
-      y="55%" 
-      className={`${styles.bar__text} 
-      ${isAnimating ? styles.bar__textAnimated : ''}`} 
-      dominantBaseline="middle" 
-      textAnchor="middle">
-        <tspan className={`${isAnimating ? styles.blurText : styles.noBlur}`}>{progress}</tspan>
+      <text
+        x="50%"
+        y="55%"
+        className={`${styles.bar__text} 
+        ${isAnimating ? styles.bar__textAnimated : ''}`}
+        dominantBaseline="middle"
+        textAnchor="middle"
+        filter={isAnimating ? 'url(#blurFilter)' : 'none'}
+      >
+        <tspan>{progress}</tspan>
       </text>
     </svg>
   );
 };
 
 export default CircularProgressBar;
-
-// import { useState, useEffect, FC } from "react";
-
-// import styles from './ProgressBar.module.scss';
-
-// interface IProps {
-//   progress: number;
-// }
-
-// const CircularProgressBar: FC<IProps> = ({ progress }) => {
-//   const [offset, setOffset] = useState({ x: 50, y: 5 });
-//   const [isAnimating, setIsAnimating] = useState(true);
-
-//   useEffect(() => {
-//     if (progress !== 0) {
-//       const limitedProgress = Math.max(0, Math.min(100, progress));
-//       const angle = ((limitedProgress * 3.6) - 90) * (Math.PI / 180);
-//       const x = 50 + 45 * Math.cos(angle);
-//       const y = 50 + 45 * Math.sin(angle);
-  
-//       setIsAnimating(true);
-//       const animationTimeout = setTimeout(() => {
-//         setOffset({ x, y });
-//         setIsAnimating(false);
-//       }, 4000);
-  
-//       return () => clearTimeout(animationTimeout);
-//     }
-
-//   }, [progress]);
-
-//   return (
-//     <svg className={styles.bar} viewBox="0 0 100 100" preserveAspectRatio="xMidYMid meet">
-//       <circle className={styles.bar__innerCircle} cx="50" cy="50" r="45"></circle>
-//       <circle className={styles.bar__outerCircle} cx="50" cy="50" r="45"></circle>
-//       <circle
-//         className={`${styles.bar__progressPoint} ${isAnimating ? styles.bar__animated : ''}`}
-//         cx={offset.x}
-//         cy={offset.y}
-//         r="10"
-//       >
-//       </circle>
-//       <foreignObject x="25" y="40" width="50" height="20" className={styles.bar__textContainer}>
-//           <text className={`${styles.bar__text} ${isAnimating ? styles.bar__textAnimated : ''}`}>
-//             {progress}
-//           </text>
-//       </foreignObject>
-//     </svg>
-//   );
-// };
-
-// export default CircularProgressBar;
