ac4e7373a91ee1bf867279e4fe8c2a80ba0bb834
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-08-01 16:35:00 +0300

    feat: rotation key

--- src/components/OrientationWarning/Warning.tsx
+++ src/components/OrientationWarning/Warning.tsx
@@ -1,12 +1,16 @@
 import { FC } from "react"
 
+import { useAppSelector } from "../../services/reduxHooks";
+
 import styles from './Warning.module.scss';
 
 export const Warning: FC = () => {
+  const translation = useAppSelector(store => store.app.languageSettings);
+  
   return (
     <div className={styles.warning}>
       <p className={styles.warning__text}>
-        Please rotate your device to portrait mode for optimal game performance.
+        {translation?.rotate_device}
       </p>
     </div>
   )
