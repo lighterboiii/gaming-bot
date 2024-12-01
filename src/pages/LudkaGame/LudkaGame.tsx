425d86c9646ae07f15cbe6b7c72807590d9722e9
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-12-01 10:12:24 +0300

    fix: winner

--- src/pages/LudkaGame/LudkaGame.tsx
+++ src/pages/LudkaGame/LudkaGame.tsx
@@ -143,7 +143,6 @@ const LudkaGame: FC = () => {
           setData(res);
           break;
         case 'room_info':
-          setWinner(null);
           setLoading(false);
           setData(res);
           break;
