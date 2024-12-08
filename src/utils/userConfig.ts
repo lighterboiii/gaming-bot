9d3b71f486ba94f7bef76d9e457d099d984f351d
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-12-08 11:22:49 +0300

    fix: end dev/prod

--- src/utils/userConfig.ts
+++ src/utils/userConfig.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-// const IS_DEVELOPMENT = process.env.REACT_APP_ENV === 'development';
-const IS_DEVELOPMENT = true; 
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 interface UserConfig {
   DEV_USER_ID: number;
@@ -9,7 +8,6 @@ interface UserConfig {
 }
 
 const config: UserConfig = {
-  // DEV_USER_ID: 172359056,
   DEV_USER_ID: 5858080651,
   PROD_USER_ID: (window as any).Telegram.WebApp.initDataUnsafe?.user?.id
 };
