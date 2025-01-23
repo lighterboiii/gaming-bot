26b1293c92aebcba544cbb507b68b48c62d00695
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2025-01-23 20:05:06 +0300

    fix: add loading state after rules button click

--- src/pages/Monetka/Monetka.tsx
+++ src/pages/Monetka/Monetka.tsx
@@ -426,6 +426,7 @@ console.log(gameState);
 
   // обработчик клика по кнопке "Ознакомился"
   const handleRuleButtonClick = () => {
+    setGameState(prev => ({ ...prev, loading: true }));
     setGameRulesWatched(userId, '4');
     triggerHapticFeedback('impact', 'soft');
     setRulesShown(true);
@@ -433,9 +434,11 @@ console.log(gameState);
       getAppData(userId)
         .then((response: IAppData) => {
           dispatch(setFourthGameRulesState(response.game_rule_4_show));
+          setGameState(prev => ({ ...prev, loading: false }));
         })
         .catch((error: Error) => {
           console.error('Get user data error:', error);
+          setGameState(prev => ({ ...prev, loading: false }));
         })
     }, 1000);
   };
