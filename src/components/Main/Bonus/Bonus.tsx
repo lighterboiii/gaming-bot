52a3496b8062bdd873f182f1506ef9387af7f9ad
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2025-02-14 22:38:27 +0300

    fix: bonus handling logic and add coins support

--- src/components/Main/Bonus/Bonus.tsx
+++ src/components/Main/Bonus/Bonus.tsx
@@ -1,7 +1,7 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 
 import Button from "../..//ui/Button/Button";
-import { clearDailyBonus, setNewTokensValue } from "../../../services/appSlice";
+import { addCoins, addTokens, clearDailyBonus } from "../../../services/appSlice";
 import { useAppDispatch, useAppSelector } from "../../../services/reduxHooks";
 import { triggerHapticFeedback } from "../../../utils/hapticConfig";
 import { IBonus } from "../../../utils/types/mainTypes";
@@ -15,19 +15,23 @@ interface IProps {
 
 const DailyBonus: FC<IProps> = ({ bonus, closeOverlay }) => {
   const dispatch = useAppDispatch();
-
   const translation = useAppSelector(store => store.app.languageSettings);
-  const handleGetBonus = async (item: IBonus) => {
-    switch (item?.bonus_type) {
-      case "tokens":
-        const formattedTokens = Math.floor(bonus?.bonus_count);
-        dispatch(setNewTokensValue(formattedTokens));
-        triggerHapticFeedback('notification', 'success');
-        break;
-      default:
-        triggerHapticFeedback('notification', 'success');
-        break;
+
+  useEffect(() => {
+    return () => {
+      dispatch(clearDailyBonus());
+    };
+  }, [dispatch]);
+
+  const handleGetBonus = async () => {
+    if (bonus?.bonus_type === "tokens") {
+      const formattedTokens = Math.floor(Number(bonus?.bonus_count));
+      dispatch(addTokens(formattedTokens));
+    } else if (bonus?.bonus_type === "coins") {
+      const formattedCoins = Math.floor(Number(bonus?.bonus_count));
+      dispatch(addCoins(formattedCoins));
     }
+    triggerHapticFeedback('notification', 'success');
     closeOverlay();
     dispatch(clearDailyBonus());
   };
@@ -47,8 +51,7 @@ const DailyBonus: FC<IProps> = ({ bonus, closeOverlay }) => {
           <p className={styles.bonus__text}>{bonus?.bonus_translate}</p>
           <div className={styles.bonus__button}>
             <Button
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              handleClick={() => handleGetBonus(bonus!)}
+              handleClick={handleGetBonus}
               text={bonus?.bonus_count 
                 ? `${translation?.claim} ${bonus.bonus_count}`
                 : translation?.claim}
